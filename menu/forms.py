d56282a32a2261b580a597e206abb45ec71ed909
Puchi <Bikram@puchi.com>
2024-01-15 01:52:17 +0530

    Food Item CRUD Ffunctionalities completed

--- menu/forms.py
+++ menu/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Category
+from .models import Category, FoodItem
+from django.utils.translation import gettext_lazy as _
 
 
 class CategoryForm(forms.ModelForm):
@@ -10,3 +11,26 @@ class CategoryForm(forms.ModelForm):
             'category_name': forms.TextInput(attrs={'class': 'form-control mb-3', 'autofocus': 'True'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+class FoodItemForm(forms.ModelForm):
+    class Meta:
+        model = FoodItem
+        fields = ('category', 'food_title', 'price', 'image', 'is_available', 'description',)
+        labels={
+            "category": _("Category *"),
+            "food_title": _("Food Title *"),
+            "price": _("Price *"),
+            "description": _("Description"),
+            "image": _("Image"),
+            "is_available": _("Is Available"),
+        }
+
+        widgets = {
+            'category': forms.Select(attrs={'class': 'form-control mb-3'},),
+            'food_title': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control mb-3', 'style': 'width:20vw'}),
+            'description': forms.Textarea(attrs={'class': 'form-control mb-3'}),
+            'image': forms.FileInput(attrs={'class': 'form-control mb-3', 'style': 'font-size:0.8rem;width:20vw'}),
+            'is_available': forms.CheckboxInput(attrs={'class': 'form-check-input mb-3'}),
+        }
+        
--- vendor/views.py
+++ vendor/views.py
@@ -9,7 +9,7 @@ from .models import Vendor
 from .forms import VendorForm
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
-from menu.forms import CategoryForm
+from menu.forms import CategoryForm, FoodItemForm
 from .utils import get_vendor
 from django.template.defaultfilters import slugify
 # Create your views here.
@@ -70,7 +70,7 @@ def menu_builder(request):
 def fooditems_by_category(request, pk=None):
     vendor = get_vendor(request)
     category = get_object_or_404(Category, pk=pk)
-    fooditems = FoodItem.objects.filter(vendor=vendor, category=category)
+    fooditems = FoodItem.objects.filter(vendor=vendor, category=category).order_by('created_at')
     context = {
         'fooditems': fooditems,
         'category': category,
@@ -91,7 +91,7 @@ def add_category(request):
             try:
                 category.save()
             except IntegrityError:
-                messages.info(request, 'Category already exists')
+                messages.info(request, 'A similar Category already exists')
             else:
                 messages.success(request, f'Category - \"{category_name.upper()}\" added successfully')
             return redirect('menu_builder')
@@ -136,7 +136,6 @@ def edit_category(request, pk=None):
             except IntegrityError:
                 messages.info(request, 'A Similar Category already exists')
                 return redirect('edit_category', category.pk)
-
             else:
                 messages.success(request, 'Category updated successfully')
             return redirect('menu_builder')
@@ -152,3 +151,78 @@ def edit_category(request, pk=None):
         'category': category,
     }
     return render(request, 'vendor/edit_category.html', context)
+
+
+def add_food(request):
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            food_title = form.cleaned_data['food_title']
+            food = form.save(commit=False)
+            food.vendor = get_vendor(request)
+            food.slug = slugify(food.food_title)
+            try:
+                food.save()
+            except IntegrityError:
+                messages.info(request, 'A similar food item already exists')
+                food_category = form.cleaned_data['category']
+                return redirect('fooditems_by_category', food_category.pk)
+            else:
+                messages.success(request, 'Food Item added successfully')
+            return redirect('fooditems_by_category', food.category.pk)
+        else:
+            for field in form:
+                for error in field.errors:
+                    messages.error(request, error)
+    else:
+        form = FoodItemForm()
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
+    context = {
+        'form': form,
+    }
+    return render(request, 'vendor/add_food.html', context)
+
+def edit_food(request, pk=None):
+    food = get_object_or_404(FoodItem, pk=pk)
+    if food.vendor != get_vendor(request):
+        messages.error(request, 'You are not allowed to edit this food item')
+        return redirect('menu_builder')
+    
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST, request.FILES, instance=food)
+        if form.is_valid():
+            food_title = form.cleaned_data['food_title']
+            food = form.save(commit=False)
+            food.slug = slugify(food_title)
+            try:
+                food.save()
+            except IntegrityError:
+                messages.info(request, 'A similar food item already exists')
+                return redirect('edit_food', food.pk)
+            else:
+                messages.success(request, 'Food Item updated successfully')
+            return redirect('fooditems_by_category', food.category.pk)
+        else:
+            for field in form:
+                for error in field.errors:
+                    print(error)
+                    messages.error(request, error)
+    else:
+        form = FoodItemForm(instance=food)
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
+    context = {
+        'form': form,
+        'food': food,
+    }
+    return render(request, 'vendor/edit_food.html', context)
+
+def delete_food(request, pk=None):
+    food = get_object_or_404(FoodItem, pk=pk)
+    if food.vendor != get_vendor(request):
+        messages.error(request, 'You are not allowed to delete this food item')
+        return redirect('menu_builder')
+    
+    pk = food.category.pk
+    food.delete()
+    messages.success(request, 'Food Item deleted successfully')
+    return redirect('fooditems_by_category', pk)
